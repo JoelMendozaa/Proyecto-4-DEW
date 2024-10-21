9ffd25ca90ce9075be6fc31f6c9a8b77c5e12f20
Joel <yowismr@gmail.com>
2024-10-21 09:35:43 +0100

    Update code index style
    Se realizaron pruebas y actualizacion de code

---
+++ code.js
@@ -0,0 +1,12 @@
+function Torre (discos, origen, destino, auxiliar, resultado){
+    this.discos = discos;
+    this.origen = origen;
+    this.destino = destino;
+    this.auxiliar = auxiliar;
+    this.resultado = resultado;
+}
+
+const torre1 = new Torre ();
+const torre2 = new Torre ();
+const torre3 = new Torre ();
+
